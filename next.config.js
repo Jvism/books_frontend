2473f7661b9c20f4738df734634f2a40b09f68e1
ji <jvi.smo@gmail.com>
2023-10-27 21:18:30 -0300

    conf: add configuration image and library

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.amazonaws.com',
+        port: '',
+        pathname: '/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
